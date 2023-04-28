b2e3f6ce9b6c47680e903feba04efd5f4c3f8ca9
chgra1 <114295893+chgra1@users.noreply.github.com>
2023-04-28 17:26:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import datetime
-import matplotlib.pyplot as plt
-import seaborn as sns
-import sklearn as sk
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import confusion_matrix 
-from sklearn.ensemble import StackingClassifier
-from sklearn.model_selection import GridSearchCV
+#import datetime
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+#import sklearn as sk
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.model_selection import train_test_split
+#from sklearn.metrics import accuracy_score
+#from sklearn.neighbors import KNeighborsClassifier
+#from sklearn.model_selection import cross_val_score
+#from sklearn.metrics import confusion_matrix 
+#from sklearn.ensemble import StackingClassifier
+#from sklearn.model_selection import GridSearchCV
 
 
 ######## data manipulation
